7d839cfef21555678ef3273c258e4315e57e728e
Lewis Collard <lewiscollard@gmail.com>
2020-07-02 01:21:37 +0100

    Use a more positive subtitle.

--- exhaust/settings/base.py
+++ exhaust/settings/base.py
@@ -19,7 +19,7 @@ ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../'))
 
 SITE_DOMAIN = 'exhaust.lewiscollard.com'
 SITE_NAME = 'Exhaust'
-SITE_DESCRIPTION = 'a thought dumping ground'
+SITE_DESCRIPTION = 'thoughts on the passing scene'
 
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
