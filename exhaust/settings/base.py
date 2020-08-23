385f1d69619a0998fc8334d6db4555b74441990f
Lewis Collard <lewiscollard@gmail.com>
2020-08-23 13:55:10 +0100

    Add STATIC_ROOT.

--- exhaust/settings/base.py
+++ exhaust/settings/base.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = '/var/www/exhaust_static'
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 STATICFILES_DIRS = [os.path.join(ROOT_DIR, 'static')]
