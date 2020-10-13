923ada996e315b11a1e72c85eae1002040894f57
Lewis Collard <lewiscollard@gmail.com>
2020-10-13 20:16:51 +0100

    Never expire caches by default.

--- exhaust/settings/base.py
+++ exhaust/settings/base.py
@@ -81,6 +81,10 @@ CACHES = {
     'default': {
         'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
         'LOCATION': '127.0.0.1:11211',
+        # None = cache forever (until a code update does a memcached restart).
+        # If there's no clear plan for cache invalidation, it should not be
+        # cached.
+        'TIMEOUT': None,
     }
 }
 
