4548167c5de4f95120371290063a0232a571162c
Lewis Collard <lewiscollard@gmail.com>
2020-10-14 20:29:10 +0100

    Add test for generate_secret_key management command.

---
+++ exhaust/common/tests/test_management.py
@@ -0,0 +1,20 @@
+import os
+
+from django.conf import settings
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class ManagementCommandsTestCase(TestCase):
+    def test_generating_secret_key_works(self):
+        if os.path.exists(settings.SECRET_KEY_FILE):
+            os.unlink(settings.SECRET_KEY_FILE)
+
+        # sanity check
+        self.assertFalse(os.path.exists(settings.SECRET_KEY_FILE), False)
+
+        call_command('generate_secret_key')
+
+        self.assertTrue(os.path.exists(settings.SECRET_KEY_FILE), False)
+        with open(settings.SECRET_KEY_FILE) as fd:
+            self.assertEqual(len(fd.read().strip()), 50)
