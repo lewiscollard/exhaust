011e7e45c8140e38b76f301a4ecb263fe994059a
Lewis Collard <lewiscollard@gmail.com>
2020-10-14 18:08:53 +0100

    Add coverage (including Django template coverage).
    Side-effect: break out requirements-dev.txt from requirements.txt as the
    prod environment is containing a number of non-required things (as much
    as I'd like prod to never differ from dev).

--- exhaust/settings/local.py
+++ exhaust/settings/local.py
@@ -3,6 +3,8 @@ import os
 from .base import *  # pylint: disable=unused-wildcard-import,wildcard-import
 
 DEBUG = True
+# `debug` option must be on for the Django template coverage plugin to work.
+TEMPLATES[0]['OPTIONS']['debug'] = True
 
 MEDIA_ROOT = os.path.expanduser(os.path.join('~/Sites', SITE_DOMAIN, 'media'))
 STATIC_ROOT = os.path.expanduser(os.path.join('~/Sites', SITE_DOMAIN, 'static'))
