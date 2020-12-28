2d5f55e970cc653aaf6eb46550d8c798c6757553
Lewis Collard <lewiscollard@gmail.com>
2020-12-28 14:20:09 +0000

    On second thoughts, all warnings being on was annoying.

--- manage.py
+++ manage.py
@@ -8,7 +8,6 @@ from django.core.management import execute_from_command_line
 
 
 def main():
-    warnings.simplefilter('module')
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exhaust.settings.local')
     os.environ.setdefault('DB_USER', pwd.getpwuid(os.getuid()).pw_name)
     os.environ.setdefault('DB_NAME', 'exhaust')
