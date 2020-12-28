22791462fdd4812e6fd04c004162f31f01bdf725
Lewis Collard <lewiscollard@gmail.com>
2020-12-28 12:03:46 +0000

    Bump Django again, to the 3.x series, and fix a bunch of warnings (incl deps).

--- exhaust/posts/urls.py
+++ exhaust/posts/urls.py
@@ -1,12 +1,11 @@
-from django.conf.urls import url
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'posts'
 
 urlpatterns = [
-    url(r'^$', views.PostListView.as_view(), name='post_list'),
+    re_path(r'^$', views.PostListView.as_view(), name='post_list'),
     path('category/<slug:slug>/', views.PostCategoryListView.as_view(), name='post_category_list'),
     path('feed/', views.PostFeedView.as_view(), name='post_feed'),
     path('post/<int:identifier>/', views.PostDetailView.as_view(), name='post_detail'),
--- exhaust/urls.py
+++ exhaust/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import include, path
 from django.views.generic import TemplateView
+from markdownx.views import MarkdownifyView
 
 from exhaust.posts.sitemaps import POSTS_SITE_MAPS
 
@@ -11,7 +12,7 @@ urlpatterns = [
     path('', include('exhaust.posts.urls', namespace='posts')),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
     path('admin/', admin.site.urls),
-    path('markdownx/', include('markdownx.urls')),
+    path('markdownx/markdownify/', MarkdownifyView.as_view(), name='markdownx_markdownify'),
     path('sitemap.xml', sitemap, {'sitemaps': POSTS_SITE_MAPS}, name='django.contrib.sitemaps.views.sitemap'),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
