2b130a4cfefde1ed3193c9cdac26a8aec4c79b1d
Lewis Collard <lewiscollard@gmail.com>
2020-12-28 12:27:30 +0000

    Turn on all warnings in manage.py and tidy up a bit.

--- manage.py
+++ manage.py
@@ -1,23 +1,17 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
 import os
 import pwd
 import sys
+import warnings
+
+from django.core.management import execute_from_command_line
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "exhaust.settings.local")
-    os.environ.setdefault("DB_USER", pwd.getpwuid(os.getuid()).pw_name)
-    os.environ.setdefault("DB_NAME", "exhaust")
-
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+    warnings.simplefilter('module')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exhaust.settings.local')
+    os.environ.setdefault('DB_USER', pwd.getpwuid(os.getuid()).pw_name)
+    os.environ.setdefault('DB_NAME', 'exhaust')
     execute_from_command_line(sys.argv)
 
 
