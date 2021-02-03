7f9da8fe03813c4018f448726fadce6b3eb09fcf
Lewis Collard <lewiscollard@gmail.com>
2021-02-03 17:57:20 +0000

    async/awaitify conditional JS imports.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,14 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', async () => {
   if (document.querySelector('.vue-youtube-mount')) {
-    import('vue').then(vueModule => {
-      import('./components/youtube-video.vue').then(ytModule => {
-        for (const mountpoint of document.querySelectorAll('.vue-youtube-mount')) {
-          new vueModule.default({
-            el: mountpoint,
-            components: {
-              'youtube-video': ytModule.default,
-            }
-          })
+    const vueModule = await import('vue');
+    const youtubeModule = await import('./components/youtube-video.vue');
+    for (const mountpoint of document.querySelectorAll('.vue-youtube-mount')) {
+      new vueModule.default({
+        el: mountpoint,
+        components: {
+          'youtube-video': youtubeModule.default,
         }
       })
-    })
+    }
   }
 });
