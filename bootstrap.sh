1aee9d7101876f291e85ba7ccda80a37bc4ed6a7
Lewis Collard <lewiscollard@gmail.com>
2021-03-28 14:19:33 +0100

    Fix bootstrap.sh: fix chicken/egg with .secret_key, make sure right node is used, install yarn if it's not already.
    Also, fix some things shellcheck didn't like. Lint all of the things.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
-# Quickstart initialisation.
+# Quickstart for a fresh machine. Requires a modernish Python and nvm.
+set -e
 rm -rf .venv
 rm -rf node_modules
-virtualenv -p python3.6 .venv
+virtualenv -p python3 .venv
+# shellcheck disable=1091
 . .venv/bin/activate
 pip install -r requirements-dev.txt
+# shellcheck disable=1090
+. ~/.nvm/nvm.sh
+nvm install "$(cat .nvmrc)"
+npm install -g yarn
 yarn
-./manage.py generate_secret_key
+# We have `manage.py generate_secret_key`, but that loads Django settings,
+# which depends on the existence of `.secret_key`!
+openssl rand -hex 64 > .secret_key
