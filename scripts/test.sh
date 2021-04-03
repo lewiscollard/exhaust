d3b6965e7a0b474556be6c602caa89f616bd1a36
Lewis Collard <lewiscollard@gmail.com>
2021-04-03 12:49:49 +0100

    Move testing out of tox and into scripts/test.sh.

---
+++ scripts/test.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+coverage run --source=exhaust/ ./manage.py check --deploy --settings=exhaust.settings.production --fail-level=WARNING
+coverage run --append --source=exhaust/ ./manage.py test
+# Ensure there are no model changes that should have a migration.
+./manage.py makemigrations --check --dry-run
+flake8 exhaust/
+pylint --rcfile=tox.ini exhaust/
+isort --check-only --diff exhaust/
+coverage report --show-missing
