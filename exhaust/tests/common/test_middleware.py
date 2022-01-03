ac932ebd9a448ac246b4b58c1efb01d841d6f27d
Lewis Collard <lewiscollard@gmail.com>
2022-01-03 06:24:41 +0000

    Cover the "disable CSP with environment variable" branch of CSPMiddleware.

--- exhaust/tests/common/test_middleware.py
+++ exhaust/tests/common/test_middleware.py
@@ -1,3 +1,5 @@
+from test.support import EnvironmentVarGuard
+
 from django.test import TestCase
 
 
@@ -5,4 +7,12 @@ class MiddlewareTestCase(TestCase):
     def test_csp_header_is_added(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        assert 'Content-Security-Policy' in response.headers
+        self.assertIn('Content-Security-Policy', response.headers)
+
+    def test_csp_middleware_not_used_with_env_var_set(self):
+        env = EnvironmentVarGuard()
+        env.set('EXHAUST_DISABLE_CSP', '1')
+
+        with env:
+            response = self.client.get('/')
+        self.assertNotIn('Content-Security-Policy', response.headers)
